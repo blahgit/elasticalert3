f0a8e8c0b7b95097783475e0d4552109ff8876e2
intovault@outlook.com <intovault@outlook.com>
2016-09-12 16:22:06 +0530

    test 7

--- runner.py
+++ runner.py
@@ -7,7 +7,7 @@ if __name__=="__main__":
 	if count:
 		os.system("python %s/elastalert_ui/ESAlerter.py &" %(base_dir))
 	count=os.system("ps -ef |grep -v 'grep' | grep -c 'elastalert.elastalert'")
-    print count
+	print count
 	if count:
 		os.system("cd %s/elastalert; python -m elastalert.elastalert --verbose &" %(base_dir))
 	print "Done."
